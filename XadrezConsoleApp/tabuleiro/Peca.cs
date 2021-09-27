941c186dd1994105842c642920eb2534635909d6
André Ferrari Picolli <picolli@outlook.com.br>
2021-09-27 16:48:26 -0300

    Começando a implementar o tabuleiro

---
+++ XadrezConsoleApp/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int quantidadeMovimentos {get; set;}
+        public Tabuleiro tabuleiro { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tabuleiro = tabuleiro;
+            this.cor = cor;
+            this.quantidadeMovimentos = 0;
+        }
+
+    }
+}
