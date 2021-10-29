82c598fa0c4b4d568f30f3236b15b9a6acbd1e6d
André Ferrari Picolli <picolli@outlook.com.br>
2021-10-28 22:53:03 -0300

    Jogada especial roque pequeno e roque grande

--- XadrezConsoleApp/xadrez/PartidaDeXadrez.cs
+++ XadrezConsoleApp/xadrez/PartidaDeXadrez.cs
@@ -34,6 +34,28 @@ namespace xadrez
             {
                 capturadas.Add(pecaCapturada);
             }
+
+            // #jogadaespecial roque pequeno
+            if(peca is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna + 1);
+                Peca Torre = tabuleiro.retirarPeca(origemTorre);
+                Torre.incrementarMovimento();
+                tabuleiro.colocarPeca(Torre, destinoTorre);
+
+            }
+
+            // #jogadaespecial roque grande
+            if (peca is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna - 1);
+                Peca Torre = tabuleiro.retirarPeca(origemTorre);
+                Torre.incrementarMovimento();
+                tabuleiro.colocarPeca(Torre, destinoTorre);
+
+            }
             return pecaCapturada;
         }
 
@@ -47,6 +69,26 @@ namespace xadrez
                 capturadas.Remove(pecaCapturada);
             }
             tabuleiro.colocarPeca(p, origem);
+
+            // #jogadaespecial roque pequeno
+            if (p is Rei && destino.coluna == origem.coluna + 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna + 3);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna + 1);
+                Peca Torre = tabuleiro.retirarPeca(destinoTorre);
+                Torre.decrementarMovimento();
+                tabuleiro.colocarPeca(Torre, origemTorre);
+            }
+            // #jogadaespecial roque grande
+            if (p is Rei && destino.coluna == origem.coluna - 2)
+            {
+                Posicao origemTorre = new Posicao(origem.linha, origem.coluna - 4);
+                Posicao destinoTorre = new Posicao(origem.linha, origem.coluna - 1);
+                Peca Torre = tabuleiro.retirarPeca(destinoTorre);
+                Torre.decrementarMovimento();
+                tabuleiro.colocarPeca(Torre, origemTorre);
+
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
@@ -232,7 +274,7 @@ namespace xadrez
             colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.Branca));
             colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.Branca));
             colocarNovaPeca('d', 1, new Dama(tabuleiro, Cor.Branca));
-            colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.Branca));
+            colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.Branca, this));
             colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.Branca));
             colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.Branca));
             colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.Branca));
@@ -251,7 +293,7 @@ namespace xadrez
             colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.Preta));
             colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.Preta));
             colocarNovaPeca('d', 8, new Dama(tabuleiro, Cor.Preta));
-            colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.Preta));
+            colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.Preta, this));
             colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.Preta));
             colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.Preta));
             colocarNovaPeca('h', 8, new Torre(tabuleiro, Cor.Preta));
--- XadrezConsoleApp/xadrez/Rei.cs
+++ XadrezConsoleApp/xadrez/Rei.cs
@@ -4,8 +4,11 @@ namespace xadrez
 {
     class Rei : Peca
     {
-        public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        private PartidaDeXadrez partida;
+
+        public Rei(Tabuleiro tabuleiro, Cor cor, PartidaDeXadrez partida) : base(tabuleiro, cor)
         {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -17,6 +20,12 @@ namespace xadrez
         {
             Peca p = tabuleiro.peca(posicao);
             return p == null || p.cor != cor;
+        }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tabuleiro.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.quantidadeMovimentos == 0;
         }
 
         public override bool[,] movimentosPossiveis()
@@ -73,6 +82,41 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            // #jogadaespecial roque
+
+            if (quantidadeMovimentos == 0 && !partida.xeque)
+            {
+                // #jogadaespecial roque pequeno
+                Posicao PosicaoTorre1 = new Posicao(posicao.linha, posicao.coluna + 3);
+
+                if (testeTorreParaRoque(PosicaoTorre1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+
+                    if(tabuleiro.peca(p1) == null && tabuleiro.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                // #jogadaespecial roque grande
+                Posicao PosicaoTorre2 = new Posicao(posicao.linha, posicao.coluna - 4);
+
+                if (testeTorreParaRoque(PosicaoTorre2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+
+                    if (tabuleiro.peca(p1) == null && tabuleiro.peca(p2) == null && tabuleiro.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
             return mat;
         }
     }
